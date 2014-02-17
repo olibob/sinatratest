3a89553adc043459cacabb89822a6d1f8200314a
Olibob <robby57@gmail.com>
2014-02-17 18:15:09 +0100

    Add heroku database configuration

--- main.rb
+++ main.rb
@@ -2,6 +2,14 @@ require 'sinatra'
 #require 'sinatra/reloader' if development?
 require './song'
 
+configure :development do
+	DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/development.db")
+end
+
+configure :production do
+	DataMapper.setup(:default, ENV['DATABASE_URL'])
+end
+
 configure do
 	enable :sessions
 	set :session_secret, '12345678900987654322345326576982757865241'
